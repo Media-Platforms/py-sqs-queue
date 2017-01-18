8edfcccc2a698cac2bce2c7c8ec27fe85bb223d0
Nic Wolff <nwolff@hearst.com>
2017-01-18 15:25:31 -0500

    Add MANIFEST.in so README.md works, use py_modules in setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 try:
     from pypandoc import convert
-    long_description = convert('README.md', 'rst')
+    long_description = convert('README.md', 'rst', 'md')
 except(IOError, ImportError):
     long_description = open('README.md').read()
 
 
 setup(
     name='sqs_queue',
-    version='0.1.0',
+    version='0.1.2',
     description='AWS SQS queue consumer/publisher',
     author='Nic Wolff',
     author_email='nwolff@hearst.com',
     license='MIT',
     long_description=long_description,
     url='http://github.com/HearstCorp/py-sqs-queue',
-    packages=find_packages(),
+    py_modules=['sqs_queue'],
     install_requires=['boto3']
 )
