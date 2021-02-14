6ab95848d6389fddd6e71e31acc366306ac02039
Nic Wolff <nwolff@hearst.com>
2021-02-13 21:18:05 -0500

    Finish processing current message before exiting on SIGTERM

--- sqs_queue.py
+++ sqs_queue.py
@@ -1,5 +1,6 @@
 import json
 from logging import getLogger
+from signal import SIGTERM, getsignal, signal
 from time import sleep
 
 import boto3
@@ -8,8 +9,10 @@ logger = getLogger(__name__)
 
 
 class Queue(object):
+    got_sigterm = False
 
-    def __init__(self, queue_name, poll_wait=20, poll_sleep=40, sns=False, drain=False, **kwargs):
+    def __init__(self, queue_name, poll_wait=20, poll_sleep=40, sns=False, drain=False,
+                 trap_sigterm=True, **kwargs):
         sqs = boto3.resource('sqs')
         self.queue = sqs.get_queue_by_name(QueueName=queue_name, **kwargs)
         self.poll_wait = poll_wait
@@ -17,12 +20,15 @@ class Queue(object):
         self.sns = sns
         self.drain = drain
 
+        if trap_sigterm:
+            signal(SIGTERM, self.make_sigterm_handler())
+
     def __iter__(self):
         self.consumer = self.queue_consumer()
         return self.consumer
 
     def queue_consumer(self):
-        while True:
+        while not self.got_sigterm:
             messages = self.queue.receive_messages(WaitTimeSeconds=self.poll_wait)
             for message in messages:
                 try:
@@ -51,8 +57,21 @@ class Queue(object):
                     return
                 sleep(self.poll_sleep)
 
+        logger.info('Got SIGTERM earlier, finished processing message and now exiting')
+
     def publish(self, body, **kwargs):
         self.queue.send_message(MessageBody=body, **kwargs)
+
+    def make_sigterm_handler(self):
+        existing_handler = getsignal(SIGTERM)
+
+        def set_terminate_flag(signum, frame):
+            logger.info('Got SIGTERM, will exit after processing this message')
+            self.got_sigterm = True
+            if callable(existing_handler):
+                existing_handler(signum, frame)
+
+        return set_terminate_flag
 
 
 class Message(dict):
