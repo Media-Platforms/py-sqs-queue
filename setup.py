18727e132d14e17e63ab0ff83c7420010230e2c5
Nic Wolff <nwolff@hearst.com>
2026-02-10 19:16:54 -0500

    Oops, bump version to 0.6.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sqs_queue',
-    version='0.6.6',
+    version='0.6.7',
     description='AWS SQS queue consumer/publisher',
     author='Nic Wolff',
     author_email='nwolff@hearst.com',
